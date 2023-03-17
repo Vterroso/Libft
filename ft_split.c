37cf14fa2d0438c3ea73289ab5820bdd6bcaaeb8
vterroso <v.terroso87@gmail.com>
2023-03-17 11:06:10 +0100

    update split, rm a.out

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: vterroso <vterroso@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:51:18 by vterroso          #+#    #+#             */
-/*   Updated: 2023/03/16 16:30:35 by vterroso         ###   ########.fr       */
+/*   Updated: 2023/03/17 11:04:59 by vterroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 int	count_words(char const *s, char c)
 {
 	int	count;
+	int	i;
 
 	count = 0;
-	while (*s)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		while (*s == c)
-			s++;
-		if (*s)
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
 			count++;
-		while (*s && *s != c)  
-			s++;
+		while (s[i] != '\0' && s[i] != c)  
+			i++;
 		
 	}
 	return (count);
@@ -40,7 +42,7 @@ char	**ft_split(char const *s, char c)
 int main(void)
 {
     char *str = "Hola amigo mio";
-    char c = 40;
+    char c = ' ';
     printf("%d\n", count_words(str, c));
 
     return 0;
